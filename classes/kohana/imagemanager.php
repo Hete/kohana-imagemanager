ddd64e176d7df0065a85ac3c1dceda5dbd98c039
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2012-07-21 08:00:30 -0400

    Added functions. delete() remove all unreferenced columns of an image in the database. retreive() returns an ORM based on a table name and a id. retreive_from_model() does the same thing but only takes a model in input.

--- classes/kohana/imagemanager.php
+++ classes/kohana/imagemanager.php
@@ -1,187 +1,158 @@
-<?php 
+<?php
 
 abstract class Kohana_ImageManager {
 
+	/**
+	 *
+	 */
+	protected static $_instance;
+	protected $_config;
 
-
-    /**
-     *
-     */
-    protected static $_instance;
-    
-    protected $_config;
-    
-    public static function instance() {
-        return ImageManager::$_instance ? ImageManager::$_instance : ImageManager::$_instance = new ImageManager();
-    }
-
-    private function __construct() {
-       
-        $this->_config = Kohana::$config->load('imagemanager.default');
-
-	// Initial tests
-	if( ! is_writable($this->_config['base_path'])) {
-		throw new Kohana_Exception("Image folder (:path) folder not writable.", array(":path" => $this->_config['base_path']));
+	public static function instance() {
+		return ImageManager::$_instance ? ImageManager::$_instance : ImageManager::$_instance = new ImageManager();
 	}
 
-        
+	private function __construct($config = 'default') {
 
-	// We need ORM, database and image modules to work correctly
-        
+		$this->_config = Kohana::$config->load("imagemanager.$config");
 
-        // Let's check the integrety of the database.
-        $this->check_integrity();
-        
-    }    
-
-
-    //////////////////////
-    // Storage functions
-
-    /**
-     * Store an image data on hard drive and database.
-     * @param $image_path Path to the image to store.
-     * @param $update Tells if we have to update the 
-     * @param $parent_table Table to which the image is associated.
-     * @param $parent_id Id to which the image is associated.
-     * @throws Image_Manager_Invalid_Hash_Exception if hash from file and hash from image_data do not match.
-     * @return the corresponding ORM model for this image.
-     */
-    public function store($image_path, $update = true, $parent_table = null, $parent_id = null) {              
-        
-        $hash = sha1_file($image_path);
-        
-        $filename = $this->filename_from_hash($hash);     
-
-        if ( ! $this->image_exists($hash)) {
-        
-        	// On déplace l'image
-            if ( ! move_uploaded_file($image_path, $filename)) {
-        	throw new Kohana_Exception("Image copy from $image_path to $filename has failed !");
-            } 
-        	
-            // Test de validité
-            if(sha1_file($filename) !== $hash) {
-                unlink($filename);
-                throw new Kohana_Exception("Hash calculated from store parameter and file do not match.");
-            }        
-        
-        }      
-
-        if($update) $this->_update_checksum(); 
-        
-
-        $image = ORM::factory('image');
-        $image->hash = $hash;
-        $image->parent_id = $parent_id;
-        $image->parent_table = $parent_table;        
-        $image->save();       
-        
-        return $hash;
-        
-    }
- 
-    
-    
-    /**
-     * Store images from the $_FILES['<html name attribute>'] variable
-     */  
-    public function store_files($files, $parent_table = null, $parent_id = null) {
-	
-    	foreach ($files["tmp_name"] as $filepath) { 
-    		if($filepath === "") continue;     			
-    		
-    		// $filepath = $files["tmp_name"][$key];
-        	ImageManager::instance()->store($filepath, false, $parent_table, $parent_id);
-        	unset($filepath);    			
+		// Initial tests
+		if (!is_writable($this->_config['base_path'])) {
+			throw new Kohana_Exception("Image folder (:path) folder not writable.", array(":path" => $this->_config['base_path']));
 		}
-	$this->_update_checksum();
-    }
-    
-      /**
-       * @deprecated
-       */
-    public function store_from_files_variable($files, $parent_table = null, $parent_id = null) {
-        $this->store_files($files, $parent_table, $parent_id);
-    		
-    }  
+	}
 
-    /**
-     * Delete an unreferenced ($parent_id and $parent_table must be null) image corresponding to the $hash.
-     * Only works if the image is not referenced in the database or if $force is true.
-     */
-    public function delete($hash) {
-        if(! $this->image_exists($hash)) {
-            throw new Kohana_Exception(":hash do not exists in images folder !", array(":hash" => $hash));        
-        }
-    }
+	//////////////////////
+	// Storage functions
 
-    ////////////////////////////
-    // Utilities
+	/**
+	 * Store an image data on hard drive and database.
+	 * @param $image_path Path to the image to store.
+	 * @param $update Tells if we have to update the 
+	 * @param $parent_table Table to which the image is associated.
+	 * @param $parent_id Id to which the image is associated.
+	 * @throws Image_Manager_Invalid_Hash_Exception if hash from file and hash from image_data do not match.
+	 * @return the corresponding ORM model for this image.
+	 */
+	public function store($image_path, $parent_table = null, $parent_id = null) {
 
-    /**
-     * Update the checksum.
-     */
-    private function _update_checksum() {} 
+		$hash = sha1_file($image_path);
 
-    /**
-     * Returns the current checksum (not calculated).
-     */
-    private function _current_checksum() {}
+		$filename = $this->filepath_to_hash($hash);
 
-    public function filename_from_hash($hash) {
-    	return $this->_config['base_path'] . "/$hash";
-    }
+		if (!$this->image_exists($hash)) {
 
-    /**
-     * Check the integrity.
-     * If a row in the database is missing, it adds it with both $parent_id and $parent_name setted to null.
-     * If a file is missing, it throws an exception.
-     * Compares the checksum with the older checksum.
-     * @return an associative array with state informations.
-     * 'num_of_files'
-     * 'num_of_rows'
-     * 'checksum' sha1 of
-     */
-    public function check_integrity() {
-        if($this->_current_checksum() !== ($new_checksum = $this->_update_checksum())) {
-            throw new Kohana_Exception("Images checksum has changed. Please verify the integrity manually.");
-        }
+			// On déplace l'image
+			if (!move_uploaded_file($image_path, $filename)) {
+				throw new Kohana_Exception("Image copy from $image_path to $filename has failed !");
+			}
+
+			// Test de validité
+			if (sha1_file($filename) !== $hash) {
+				unlink($filename);
+				throw new Kohana_Exception("Hash calculated from store parameter and file do not match.");
+			}
+		}
 
 
 
 
-	return array(
-            'checksum' => $new_checksum
-        )
-    } 
+
+		$image = ORM::factory('image');
+		$image->hash = $hash;
+		$image->parent_id = $parent_id;
+		$image->parent_table = $parent_table;
+		$image->save();
+
+		return $hash;
+	}
+
+	/**
+	 * Store images from the $_FILES['<html name attribute>'] variable
+	 */
+	public function store_files($files, $parent_table = null, $parent_id = null) {
+
+		foreach ($files["tmp_name"] as $filepath) {
+			if ($filepath === "")
+				continue;
+
+			// $filepath = $files["tmp_name"][$key];
+			ImageManager::instance()->store($filepath, false, $parent_table, $parent_id);
+			unset($filepath);
+		}
+		$this->_update_checksum();
+	}
+
+	/////////////////////
+	// Retreive functions
+
+	/**
+	 * Get a fetchable array of images that belong to a model.
+	 * @param type $parent_table
+	 * @param type $parent_id
+	 */
+	public function retreive($parent_table = null, $parent_id = null) {
+		return ORM::factory('image')
+				->where('parent_table', '=', $parent_table)
+				->and_where('parent_id', '=', $parent_id);
+	}
+
+	/**
+	 * Get a fetchable array of images that belong to a model.
+	 * @param type $parent_table
+	 * @param type $parent_id
+	 */
+	public function retreive_from_model($model) {
+
+		return retreive($model->_table_name, $model->pk());
+	}
+
+	//////////////////
+	// Delete functions
+
+	/**
+	 * Delete an unreferenced ($parent_id and $parent_table must be null) image corresponding to the $hash.
+	 * Only works if the image is not referenced in the database or if $force is true.
+	 */
+	public function delete($hash) {
+		if (!$this->image_exists($hash)) {
+			throw new Kohana_Exception(":hash do not exists in images folder !", array(":hash" => $hash));
+		}
+		
+		// Test de référencement
+		foreach (ORM::factory('image', array('hash' => $hash))->find_all() as $image) {
+			
+			if( ! ORM::factory($image->_table_name, $image->pk())->find()) {
+				$image->delete();				
+			}
+			
+		}
+		
+		if(ORM::factory('image', array('hash' => $hash))->find_all()->count_all() < 1) {
+			// Aucuns modèles ne référence cette image, elle peut être détruite.
+			
+		}
+	}
+
+	////////////////////////////
+	// Utilities
 
 
 
-    /**
-     * Garebage collector.
-     * Finds unreferenced files in the images folder and compress them to save space.
-     * @param $delete Delete instead of compressing.
-     */
-    public function gc($delete = false) {} 
-    
-    
-    /**
-     * Lookup the database and the files to see if the image exists.
-     */
-    public function image_exists($hash) {    	
-        return file_exists($this->filename_from_hash($hash)) and 
-            ORM::factory('image')
-                ->where('hash', '=', $hash)
-                ->find_all()
-                ->count_all() > 0;
-    }
-    
-    
+	public function hash_to_filepath($hash) {
+		return $this->_config['base_path'] . "/$hash";
+	}
 
-    
-
-
+	/**
+	 * Lookup the database and the files to see if the image exists.
+	 */
+	public function image_exists($hash) {
+		return file_exists($this->filename_from_hash($hash)) and
+				ORM::factory('image')
+				->where('hash', '=', $hash)
+				->find_all()
+				->count_all() > 0;
+	}
 
 }
 
--- classes/model/image/image.php
+++ classes/model/image/image.php
@@ -1,26 +1,28 @@
 <?php
 
-abstract class Model_Image_Image extends ORM {    
+abstract class Model_Image_Image extends ORM {
 
-    protected $_belongs_to = array(
-        $this->parent_table => array('foreign_key' => $this->parent_id)
-    );
+	/**
+	 * Retrieve the Image object associated to this model to apply transformations.
+	 * The image object is defined in the image module.
+	 * DO NOT OVERRIDE THE ORIGINAL IMAGE !
+	 */
+	public function image() {
+		return Image::factory($this->filepath());
+	}
+	
+	public function delete() {
+		ImageManager::instance()->delete($this->hash);
+		parent::delete();
+	}
 
-    /**
-     * Retrieve the Image object associated to this model.
-     * The image object is defined in the image module.
-     * DO NOT OVERRIDE THE ORIGINAL IMAGE !
-     */
-    public function image() {        
-        return Image::factory($this->filepath());
-    }
-    
-    /**
-     * Returns the path to this image.
-     */
-    public function filepath() {
-    	return ImageManager::instance()->retreive($this->hash);
-    }
-    
-    }
+	/**
+	 * Returns the path to this image.
+	 */
+	public function filepath() {
+		return ImageManager::instance()->retreive($this->hash);
+	}
+
+}
+
 ?>
