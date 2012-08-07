dd21dab30ec9d4bb1c16b7fa735def7deef07472
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2012-08-07 12:02:00 -0400

    Initial commit.

---
+++ classes/model/image.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+
+class Model_Image extends Model_Image_Image {   }
+
+?>
---
+++ config/imagemanager.php
@@ -0,0 +1,17 @@
+<?php
+
+
+return array(
+             
+    'default' => array(
+        
+            'image_table'  => 'images', // You should have an images table in your database
+            'base_path'    => 'images', // Base path where images are stored.
+            'hash_function' => 'sha1', // You may select a custon hash function           
+        
+    ),
+    'custom' => null, 
+);
+
+
+?>
