36b51f5eb4a9ef7e2bd8bb7b5f1f1cd63b065e2d
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2013-02-05 16:56:30 -0500

    Useless entry in config.

--- config/imagemanager.php
+++ config/imagemanager.php
@@ -4,7 +4,6 @@ return array(
     'default' => array(
         'image_table' => 'images', // You should have an images table in your database
         'base_path' => 'images', // Base path where images are stored.
-        'hash_function' => 'sha1', // You may select a custon hash function   
         "fallback_image" => NULL, // Fallback uri if image is not found
         'max_size' => '1M',
     ),
