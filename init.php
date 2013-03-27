158c19a8e0499a2ff5bdb691a8d5464f57df0351
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2013-03-26 22:42:32 -0400

    Wrong path for base_path in non-writeable error.

--- init.php
+++ init.php
@@ -2,6 +2,6 @@
 
 // Initial tests
 if (!is_writable(DOCROOT . Kohana::$config->load("imagemanager.base_path"))) {
-    throw new Kohana_Exception("Image folder :path folder not writable.", array(":path" => Kohana::$config->load("imagemanager.default.base_path")));
+    throw new Kohana_Exception("Image folder :path folder not writable.", array(":path" => Kohana::$config->load("imagemanager.base_path")));
 }
 ?>
