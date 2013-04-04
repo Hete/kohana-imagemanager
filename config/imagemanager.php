64a9666576177156e00c27fc0c2bdc5f25b91ccb
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2013-04-04 18:04:42 -0400

    Absolute path is a bad thing :(
    Signed-off-by: Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>

--- config/imagemanager.php
+++ config/imagemanager.php
@@ -4,7 +4,7 @@ defined('SYSPATH') or die('No direct access allowed.');
 
 return array(
     'image_table' => 'images', // You should have an images table in your database
-    'base_path' => DOCROOT . 'images', // Base path where images are stored.
+    'base_path' => 'images', // Base path where images are stored.
     "fallback_image" => NULL, // Fallback uri if image is not found
     'max_size' => '1M', // Maximum size for images
 );
